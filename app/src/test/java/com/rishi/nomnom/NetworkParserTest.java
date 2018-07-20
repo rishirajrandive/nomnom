0d37890e21d650d3b2d59f7f98f06a9611204c6e
Rishiraj Randive <rishiraj.randive@onepeloton.com>
2018-07-19 21:20:06 -0400

    Remove more tests

--- app/src/test/java/com/rishi/nomnom/NetworkParserTest.java
+++ app/src/test/java/com/rishi/nomnom/NetworkParserTest.java
@@ -34,26 +34,26 @@ public class NetworkParserTest {
         Assert.assertEquals(response.getNextPageToken(), "CqQCGwEAAI87gNq3wZ43d_70SVL6S2KwyomUi7ZwRCTTWofU1x-Ys6PcaICqSO2FYGoT1NGtZ3Dn0-De0t8z8FN74oyHX9gA_YNfMmiDoyC6SnHKTe6-eMQJrL_E8H7YdcSyZH3Mco_AtWcAJ1USuhWYwI10rh0vlTYUOE5wu9LDBFTSi639MvZIEp6OCixH_WWx0OWqKtq2st1Z8EB451XE0tluUfmo5yBty2CApztzfWSSUi68bLiboTCVFyqhwHYCV7zg5xCY_Yi_AwG2G9YV6EROcsb4zUKcOw7mEeW8ZQaCf337e3FV_8iWx9S1y7t1zejle8mTLSAiw449Fv2I6rMq3oFcT_aAh36q0SDDipV_9DTFfY9YwVnHoxb73xBswoXG5RIQa0FesftDm3DWCLA7UGojzBoUnqZdtm-0RxSxCmqkGvaii8qWmVU");
     }
 
-    @Test
-    public void testRestaurantDetailResponse() {
-        ApiResponse response = MockResponseStore.getMockRestaurantDetails();
-        Assert.assertEquals(response.getStatus(), "OK");
-        Assert.assertNull(response.getNextPageToken());
-    }
-
-    @Test
-    public void testNoResults() {
-        ApiResponse response = MockResponseStore.getMockNoResults();
-        Assert.assertEquals(response.getStatus(), "ZERO_RESULTS");
-    }
-
-    @Test
-    public void testRestaurantListParser() {
-        List<Restaurant> restaurants = networkResponseParser.getRestaurants(MockResponseStore.getMockRestaurants());
-        Assert.assertEquals(restaurants.size(), 2);
-        Validator.verifyFirstRestaurant(restaurants.get(0));
-        Validator.verifySecondRestaurant(restaurants.get(1));
-    }
+//    @Test
+//    public void testRestaurantDetailResponse() {
+//        ApiResponse response = MockResponseStore.getMockRestaurantDetails();
+//        Assert.assertEquals(response.getStatus(), "OK");
+//        Assert.assertNull(response.getNextPageToken());
+//    }
+//
+//    @Test
+//    public void testNoResults() {
+//        ApiResponse response = MockResponseStore.getMockNoResults();
+//        Assert.assertEquals(response.getStatus(), "ZERO_RESULTS");
+//    }
+//
+//    @Test
+//    public void testRestaurantListParser() {
+//        List<Restaurant> restaurants = networkResponseParser.getRestaurants(MockResponseStore.getMockRestaurants());
+//        Assert.assertEquals(restaurants.size(), 2);
+//        Validator.verifyFirstRestaurant(restaurants.get(0));
+//        Validator.verifySecondRestaurant(restaurants.get(1));
+//    }
 
 //    @Test
 //    public void testRestaurantDetailParser() {
