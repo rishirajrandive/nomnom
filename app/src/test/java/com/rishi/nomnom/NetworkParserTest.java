86fe3c365780405f2dc73890506e0b6a5d6b47fe
Rishiraj Randive <rishiraj.randive@onepeloton.com>
2018-07-19 21:17:09 -0400

    Remove one test to check coverage change

--- app/src/test/java/com/rishi/nomnom/NetworkParserTest.java
+++ app/src/test/java/com/rishi/nomnom/NetworkParserTest.java
@@ -55,22 +55,22 @@ public class NetworkParserTest {
         Validator.verifySecondRestaurant(restaurants.get(1));
     }
 
-    @Test
-    public void testRestaurantDetailParser() {
-        Restaurant restaurantDetail = networkResponseParser.getRestaurantDetail(MockResponseStore.getMockRestaurantDetails());
-        Assert.assertNotNull(restaurantDetail);
-
-        Assert.assertEquals(restaurantDetail.getFormattedAddress(), "1658 Market St, San Francisco, CA 94102, USA");
-        Assert.assertEquals(restaurantDetail.getFormattedPhone(), "(415) 552-2522");
-        Assert.assertEquals(restaurantDetail.getWebsiteUrl(), "http://www.zunicafe.com/");
-
-        Assert.assertNotNull(restaurantDetail.getOpenHours());
-        Validator.verifyDetailOpenHours(restaurantDetail.getOpenHours());
-
-        Assert.assertEquals(restaurantDetail.getPhotoList().size(), 10);
-        Validator.verifyDetailPhoto(restaurantDetail.getPhotoList().get(1)); // Just verifying one photo
-
-        Assert.assertEquals(restaurantDetail.getReviews().size(), 5);
-        Validator.verifyFirstReview(restaurantDetail.getReviews().get(0));
-    }
+//    @Test
+//    public void testRestaurantDetailParser() {
+//        Restaurant restaurantDetail = networkResponseParser.getRestaurantDetail(MockResponseStore.getMockRestaurantDetails());
+//        Assert.assertNotNull(restaurantDetail);
+//
+//        Assert.assertEquals(restaurantDetail.getFormattedAddress(), "1658 Market St, San Francisco, CA 94102, USA");
+//        Assert.assertEquals(restaurantDetail.getFormattedPhone(), "(415) 552-2522");
+//        Assert.assertEquals(restaurantDetail.getWebsiteUrl(), "http://www.zunicafe.com/");
+//
+//        Assert.assertNotNull(restaurantDetail.getOpenHours());
+//        Validator.verifyDetailOpenHours(restaurantDetail.getOpenHours());
+//
+//        Assert.assertEquals(restaurantDetail.getPhotoList().size(), 10);
+//        Validator.verifyDetailPhoto(restaurantDetail.getPhotoList().get(1)); // Just verifying one photo
+//
+//        Assert.assertEquals(restaurantDetail.getReviews().size(), 5);
+//        Validator.verifyFirstReview(restaurantDetail.getReviews().get(0));
+//    }
 }
